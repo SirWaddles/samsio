10253ff809a9fcf08f944d9fa089a62bae1cdc0b
Timothy Clissold <site@timothyclissold.com>
2018-01-30 15:01:59 +1100

    added keyed state

--- Container.js
+++ Container.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Store from './Store';
 
 class StoreContainer extends React.Component {
     constructor(props) {
         super(props);
         if (Array.isArray(props.store)) {
             this.state = props.store.reduce((acc, v) => Object.assign(acc, v.getState()), {});
-        } else {
+        } else if (props.store instanceof Store) {
             this.state = props.store.getState();
+        } else {
+            this.state = Object.keys(props.store).reduce((acc, v) => {
+                acc[v] = props.store[v].getState();
+            }, {});
         }
     }
 
@@ -24,16 +29,24 @@ class StoreContainer extends React.Component {
                 listen: v.addListener(state => this.setState(state)),
                 store: v,
             }));
-        } else {
+        } else if (this.props.store instanceof Store) {
             this.listenid = this.props.store.addListener((state) => this.setState(state));
+        } else {
+            this.listenid = Object.keys(this.props.store).map(v => ({
+                listen: this.props.store[v].addListener(state => this.setState({[v]: state})),
+                store: this.props.store[v],
+                key: v,
+            }));
         }
     }
 
     componentWillUnmount() {
         if (Array.isArray(this.props.store)) {
             this.listenid.forEach(v => v.store.removeListener(v.listen));
-        } else {
+        } else if (this.props.store instanceof Store) {
             this.props.store.removeListener(this.listenid);
+        } else {
+            this.listenid.forEach(v => v.store.removeListener(v.listen));
         }
     }
 }
